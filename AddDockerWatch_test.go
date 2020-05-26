7352c7d4d068d3c60a61e727b1bca65fd5ae57a8
tangsheng.thonsun <tangsheng.thonsun@bytedance.com>
2020-05-26 13:15:46 +0800

    init
    Change-Id: Ib18996c446096512366fc38ab666ced3b9f34a5c

--- AddDockerWatch_test.go
+++ AddDockerWatch_test.go
@@ -1,8 +1,19 @@
 package main
 
-import "testing"
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/test_fsnotigy/log"
+	"testing"
+)
 
 func TestAddDockerWatch(t *testing.T) {
+	log.SetLogLevel(log.DEBUG)
+	Watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		log.Error("fsnotify new Watcher error: %v",err)
+		return
+	}
+	defer Watcher.Close()
 	stop := make(chan int)
 	AddDockerWatch()
 	<-stop
